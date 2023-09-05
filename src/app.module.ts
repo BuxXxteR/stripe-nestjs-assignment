1cac140487665c83291e53b9545d2aa26d82a46d
nipunaDev <nipuna@sonicdevs.com>
2023-09-05 16:52:45 +0530

    commmit #02

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config/config.schema';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['src/config/config.env'],
+      validationSchema: configValidationSchema,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
