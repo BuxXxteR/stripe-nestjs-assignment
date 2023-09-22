c3c28c666e1579a319e2c73d8c2cd6b34c7dafd2
Nipuna Amaranayaka <amaranayakanipuna@gmail.com>
2023-09-22 13:54:43 +0530

    commit new chnage on webhook

--- src/paypal/paypal.service.ts
+++ src/paypal/paypal.service.ts
@@ -141,6 +141,7 @@ export class PaypalService {
 
     if (verifyWebhookSign.data.verification_status === 'SUCCESS') {
       console.log(verifyWebhookSign.data, 'verifyWebhookSign.data');
+      console.log(verifyWebhookSign.headers, 'verifyWebhookSign.headers');
       try {
         switch (verifyWebhookSign.data.body.event_type) {
           case 'BILLING.SUBSCRIPTION.CREATED':
